10ff0405ecae67ca73a2412cab04c2f5ae9e2698
CIT173F-18@M307.UC-BCF.EDU.PH <CIT173F-18@M307.UC-BCF.EDU.PH>
2022-09-15 11:12:53 +0800

    initial commit

---
+++ php1.php
@@ -0,0 +1,19 @@
+
+
+<?php 
+ 
+				echo "Twinkle, Twinkle little star. \n"; 
+				
+
+				$twinkle = "Twinkle"; 
+				$star = "star"; 
+
+				echo "$twinkle, $twinkle little $star. \n"; 
+ 
+				$twinkle = "Twinkle"; 
+				$star = "star"; 
+
+				echo "$twinkle, $twinkle little $star.";
+ 
+?>  
+
---
+++ php2.php
@@ -0,0 +1,23 @@
+
+
+<?php 
+
+        $x=8; 
+
+        echo "Value is now " . $x . "\n";
+        $x += 2;
+        echo "Add 2.\nValue is now " . $x . "\n";
+        $x -= 4;
+        echo "Subtract 4.\nValue is now " . $x . "\n";
+        $x *= 5;
+        echo "Multiply by 5.\nValue is now " . $x . "\n";
+        $x /= 3;
+        echo "Divided by 3.\nValue is now " . $x  . "\n";
+        $x = ++$x;
+        echo "Increment value by one.\nValue is now " . $x  . "\n";
+        $x = --$x;
+        echo "Decrement value by one.\nValue is now " . $x  . "\n";
+
+
+?>
+
